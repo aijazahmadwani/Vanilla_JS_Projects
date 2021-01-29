b9efc3933b6225c0d76eef9c121ce6607f6b1878
aijazahmadwani <aijazahmad9864@gmail.com>
2021-01-29 17:39:27 +0530

    project 2 half done

---
+++ Quiz App/app.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question : 'What is your name',
+        a:'Aijaz',
+        b:'Sam',
+        c:'Haider',
+        d:'Asif',
+        correct:'a'
+    },
+    {
+        question:'Best programming language ?',
+        a:'Python',
+        b:'Java',
+        c:'C++',
+        d:'JavaScript',
+        correct:'a'
+    },
+    {
+        question:'Present Year ',
+        a:'2019',
+        b:'2020',
+        c:'2021',
+        d:'2022',
+        correct:'c'
+    },
+    {
+        question:'HTML stands for ',
+        a:'Hypertext Markup Langauge',
+        b:'Cascating Style Sheet',
+        c:'Jason Object Notation',
+        d:'Application Programming Interface',
+        correct:'a'
+    }
+]
